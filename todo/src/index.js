974967c9336813aceb9285792c6aa3c95bb917e0
Nathan <s6nculve@gmail.com>
2019-05-25 10:18:53 -0700

    Fixed issue with store. Imported & passed in rootReducer in index.js.

--- todo/src/index.js
+++ todo/src/index.js
@@ -6,8 +6,9 @@ import App from './App';
 
 import { createStore } from 'redux'; 
 import { Provider } from 'react-redux';  
+import rootReducer from './reducers';
 
-const store = createStore();
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -15,7 +16,3 @@ ReactDOM.render(
   </Provider>, 
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
